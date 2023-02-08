82860606fbf898f62b8c835fe36adafe6a8b7af9
Dario La Placa <dariolaplacauni@gmail.com>
2023-02-08 23:22:41 +0100

    Updated the Symmetric Difference callings

--- src/Algorithms/SymmetricDifference.java
+++ src/Algorithms/SymmetricDifference.java
@@ -9,33 +9,35 @@ public class SymmetricDifference {
 
         //FIRST TEST
 
-        SymDiff(new ArrayList<>(Arrays.asList(3, 3, 3, 2, 5)),
+        ArrayList<Integer> firstTest = SymDiff(new ArrayList<>(Arrays.asList(3, 3, 3, 2, 5)),
                 new ArrayList<>(Arrays.asList(2, 1, 5, 7)),
                 new ArrayList<>(Arrays.asList(3, 4, 6, 6)),
                 new ArrayList<>(Arrays.asList(1, 2, 3)),
                 new ArrayList<>(Arrays.asList(5, 3, 9, 8)),
                 new ArrayList<>(Arrays.asList(1)));
-
         //should return [1, 2, 4, 5, 6, 7, 8, 9]
 
         //SECOND TEST
-        /*
-        SymDiff(new ArrayList<>(Arrays.asList(3, 3, 3, 2, 5)),
+
+        ArrayList<Integer> secondTest = SymDiff(new ArrayList<>(Arrays.asList(3, 3, 3, 2, 5)),
                 new ArrayList<>(Arrays.asList(2, 1, 5, 7)),
                 new ArrayList<>(Arrays.asList(3, 4, 6, 6)),
                 new ArrayList<>(Arrays.asList(1, 2, 3))
         );
-        */
 
         //[2, 3, 4, 6, 7].
 
         //THIRD TEST
-        /*
-        SymDiff(new ArrayList<>(Arrays.asList(1, 1, 2, 5)),
+
+        ArrayList<Integer> thirdTest =SymDiff(new ArrayList<>(Arrays.asList(1, 1, 2, 5)),
                 new ArrayList<>(Arrays.asList(2, 2, 3, 5)),
                 new ArrayList<>(Arrays.asList(3, 4, 5, 5)));
         //sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
-       */
+
+        System.out.println("Expected output: [1, 2, 4, 5, 6, 7, 8, 9] " + "\nOutput: " + firstTest);
+        System.out.println("\nExpected output: [2, 3, 4, 6, 7] " + "\nOutput: " + secondTest);
+        System.out.println("\nExpected output: [1, 4, 5] " + "\nOutput: " + thirdTest);
+
 
     }
 
@@ -57,7 +59,6 @@ public class SymmetricDifference {
             alreadyInserted.clear();
         }
         Collections.sort(finalSet);
-        System.out.println(finalSet);
         return finalSet;
     }
 }
