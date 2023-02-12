c5cbe2e316b57aad1f5a3603d95f3025752ed2b1
Dario La Placa <dariolaplacauni@gmail.com>
2023-02-12 02:40:07 +0100

    Added comments to Pairwise

--- src/Algorithms/Pairwise.java
+++ src/Algorithms/Pairwise.java
@@ -3,60 +3,54 @@ package Algorithms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-/*
-
-
- */
 public class Pairwise {
-    public int pairwise(ArrayList<Integer> arr, int targetSum){
+    public int pairwise(ArrayList<Integer> arr, int targetSum) {
+        // Create an ArrayList to hold the pairs
         ArrayList<Integer> pairsArr = new ArrayList<>();
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = 0; j < arr.size(); j++){
-                if(arr.get(i) + arr.get(j) == targetSum && i != j && !pairsArr.contains(i) && !pairsArr.contains(j)) {
+        // Looping 2 times in the same array to find the pairs that fit the condition
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = 0; j < arr.size(); j++) {
+                // Checking if the sum is equal to the targetSum, evaluating if i and j are not targeting the same index
+                // And if our pairs array doesn't contain already the index we are iterating
+                if (arr.get(i) + arr.get(j) == targetSum && i != j && !pairsArr.contains(i) && !pairsArr.contains(j)) {
+                    // In case add both index that match the condition
                     pairsArr.add(i);
                     pairsArr.add(j);
                 }
             }
         }
-        if(pairsArr.isEmpty()) {
+        // If any pair is found return 0
+        if (pairsArr.isEmpty()) {
             return 0;
         }
+        // Return the sum of all the index through a Stream Interface
         return pairsArr.stream().mapToInt(Integer::intValue).sum();
     }
 
     public static void main(String[] args) {
         Pairwise pw = new Pairwise();
         System.out.println("    TEST 1\n" +
-                        "Expected Output: 11\n" +
-                        "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 4, 2, 3, 0, 5)), 7));
+                "Expected Output: 11\n" +
+                "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 4, 2, 3, 0, 5)), 7));
 
 
         System.out.println("\n    TEST 2\n" +
-                        "Expected Output: 1\n" +
-                        "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 3, 2, 4)), 4));
+                "Expected Output: 1\n" +
+                "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 3, 2, 4)), 4));
 
 
         System.out.println("\n    TEST 3\n" +
-                        "Expected Output: 1\n" +
-                        "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 1, 1)), 2));
+                "Expected Output: 1\n" +
+                "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(1, 1, 1)), 2));
 
         System.out.println("\n  TEST 4\n" +
-                        "Expected Output: 10\n" +
-                        "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 1, 1)), 1));
+                "Expected Output: 10\n" +
+                "Output: " + pw.pairwise(new ArrayList<Integer>(Arrays.asList(0, 0, 0, 0, 1, 1)), 1));
 
         System.out.println("\n    TEST 5\n" +
-                        "Expected Output: 0\n" +
-                        "Output: " + pw.pairwise(new ArrayList<Integer>(), 100));
+                "Expected Output: 0\n" +
+                "Output: " + pw.pairwise(new ArrayList<Integer>(), 100));
 
     }
-    /*
-      pairwise([1, 4, 2, 3, 0, 5], 7) should return 11.
-      pairwise([], 4) should return 1.
-      pairwise([1, 1, 1], 2) should return 1.
-      pairwise([0, 0, 0, 0, 1, 1], 1) should return 10.
-      pairwise([], 100) should return 0.
-
-     */
 }
