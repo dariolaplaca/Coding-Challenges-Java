034335d456a2e6bcad5de594e780af2b9330058c
Dario La Placa <dariolaplacauni@gmail.com>
2023-02-15 12:19:15 +0100

    Added comments to Trapping Rain Water in misc

--- src/misc/TrappingRainWater.java
+++ src/misc/TrappingRainWater.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class TrappingRainWater {
 
     public static void main(String[] args) {
-        System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1})); // OUTPUT 6
-        System.out.println(trap(new int[]{4,2,0,3,2,5})); // OUTPUT 9
+        System.out.println("Expected Output: 6\nOutput: " + trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1})); // OUTPUT 6
+        System.out.println("Expected Output: 9\nOutput: " + trap(new int[]{4,2,0,3,2,5})); // OUTPUT 9
     }
     static int trap(int[] height) {
+        // Storing the max height (Highest value in the Array) as an int
         int maxHeight = Arrays.stream(height).max().getAsInt();
-
+        // Creating a 2d Array Map from the elevation map and storing the cells with water with a 1 and the "wall" cells with a 0
         int[][] fullMap = new int[maxHeight][height.length];
         for(int i = 0; i < fullMap.length; i++){
             for (int j = 0; j < fullMap[i].length; j++ ){
@@ -21,7 +22,8 @@ public class TrappingRainWater {
                 }
             }
         }
-
+        // System.out.println(Arrays.deepToString(fullMap));
+        // Setting all the empty cells in each row that are not constrained between a wall as 0
         for(int i = 0; i < fullMap.length; i++){
             int j = 0;
             while(j < fullMap[i].length && fullMap[i][j] == 1){
@@ -34,6 +36,8 @@ public class TrappingRainWater {
                 k--;
             }
         }
+        // System.out.println(Arrays.deepToString(fullMap));
+        // Summing all the cells in the 2d Array, only the cells containing water are numbered by one
         int waterTrapped = Arrays.asList(fullMap).parallelStream().flatMapToInt(Arrays::stream).sum();
         return waterTrapped;
     }
